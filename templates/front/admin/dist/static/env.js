36711c377ff1ffb588e8fb1a74d31c5c35643d3f
刘鑫鹏 <1503199604@qq.com>
2023-05-08 09:58:58 +0800

    env

--- templates/front/admin/dist/static/env.js
+++ templates/front/admin/dist/static/env.js
@@ -1,6 +1,6 @@
 
 
 // var DOMAIN_API_SYS = "http://127.0.0.1:8087";
-var DOMAIN_API_SYS = "http://121.36.12.82";
-// var DOMAIN_API_SYS = "http://182.42.137.129:8087";
+// var DOMAIN_API_SYS = "http://121.36.12.82";
+var DOMAIN_API_SYS = "http://182.42.126.254:8087";
 
