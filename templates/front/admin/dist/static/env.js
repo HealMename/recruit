443b022e16938cef2501b413811d497954a73e38
刘鑫鹏 <1503199604@qq.com>
2023-05-29 21:19:15 +0800

    .

--- templates/front/admin/dist/static/env.js
+++ templates/front/admin/dist/static/env.js
@@ -1,6 +1,6 @@
 
 
 // var DOMAIN_API_SYS = "http://127.0.0.1:8087";
-var DOMAIN_API_SYS = "http://121.36.12.82:8087";
+var DOMAIN_API_SYS = "http://liudeli.top:8087";
 // var DOMAIN_API_SYS = "http://182.42.126.254:8087";
 
